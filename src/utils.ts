26839c57eaf62e75de4a7997ed9d07c21717a363
Alexemarg <alexemarg.dev@gmail.com>
2021-01-01 12:17:18 +0700

    Add: arrayEach util

--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,21 @@ export function checkPointInRect(pointX: number, pointY: number, rectX1: number,
   return (pointX > rectX1 && pointX < rectX2) && (pointY > rectY1 && pointY < rectY2)
 }
 
+export function arrayEach(array: any[], iteratee: (element: any, index: number, array: any[])=>boolean) { // From lodash
+  let index = -1
+  const length = array.length
+  while (++index < length) {
+    if (iteratee(array[index], index, array) === false) {
+      break
+    }
+  }
+  return array
+}
 
 // For development
 
 export function drawAtom(ctx: CanvasRenderingContext2D, x: number, y: number, color: string) {
-  const radius = 5;
+  const radius = 3;
   ctx.beginPath();
   ctx.fillStyle = color;
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
