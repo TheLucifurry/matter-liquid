1fe4632f231dbe0b1a07f48ef11a559c40c8fa48
Alexemarg <alexemarg.dev@gmail.com>
2021-01-06 11:12:53 +0700

    Upd: add vectorLengthAdd util

--- src/utils.ts
+++ src/utils.ts
@@ -16,9 +16,25 @@ export function arrayEach(array: any[], iteratee: (element: any, index: number, 
 // For development
 
 export function drawAtom(ctx: CanvasRenderingContext2D, x: number, y: number, color: string) {
-  const radius = 3;
+  const radius = 4;
   ctx.beginPath();
   ctx.fillStyle = color;
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.fill();
+}
+
+
+// Original
+// function vectorLengthAdd(vec: [number, number], l: number){
+//   const baseLength = Math.sqrt(vec[0]**2 + vec[1]**2);
+//   const endLength = baseLength + l;
+//   const sideXproport = vec[0] / (vec[0] + vec[1]);
+//   const endSidesSum = Math.sqrt( endLength**2 / 2) * 2;
+//   const x = endSidesSum * sideXproport;
+//   return [x, endSidesSum - x];
+// }
+// Closure compiled
+export function vectorLengthAdd(vector: [number, number], a: number): [number, number] {
+  const b = 2 * Math.sqrt( (Math.sqrt(vector[0]**2 + vector[1]**2) + a) ** 2 / 2), c = vector[0] / (vector[0] + vector[1]) * b;
+  return [c, b - c];
 }
