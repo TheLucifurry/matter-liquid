69145d4d67be06c3481ee491667fb19da08b8cf2
Alexemarg <alexemarg.dev@gmail.com>
2021-01-20 19:21:30 +0700

    Upd: add "world" field to config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,18 @@
-export const Config = {
+type TConfig = {
+  w: Matter.World,
+  g: [number, number],
+  h: number,
+}
+
+export const Config: TConfig = {
+  w: null,
   g: [0, 0.01],
   h: 30,
 };
 
+export function setWorld(world: Matter.World) {
+  Config.w = world;
+}
 export function setGravity(vertical: number, horizontal: number) {
   const divider = 10;
   Config.g = [horizontal / divider, vertical / divider];
