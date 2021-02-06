5723633fef7cf29c6a99c22c0c064164c0628d2a
Alexemarg <alexemarg.dev@gmail.com>
2021-02-06 20:18:14 +0700

    Refactor index

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,11 @@ import pkg from '../package.json';
 import * as Algorithm from './algorithm';
 import * as Events from './events';
 import * as Liquid from './liquid';
-import {
-  createLiquid,
-  fillZoneByLiquid,
-  spawnLiquid
-} from './liquid';
 import * as Render from './render';
-import * as State from './state';
+import * as StateManager from './state';
 import * as Zone from './zones';
+
+const { State } = StateManager;
 
 const MatterLiquid = {
   // Original
@@ -26,9 +23,10 @@ const MatterLiquid = {
     console.log('Matter.js Plugin installed:');
     console.dir({ MatterLiquid });
 
+    const { createLiquid, spawnLiquid, fillZoneByLiquid } = Liquid;
+
     matter.liquid = {
-      Zone,
-      Config: State,
+      State,
       Events,
       createLiquid,
       spawnLiquid,
@@ -39,33 +37,39 @@ const MatterLiquid = {
     const worldWidth = 5000;
     const deltaTime = 1;
 
+    const starter = start();
+
     matter.after('Render.create', function(this: Matter.Render) {
-      console.log('Render:');
-      console.dir(this);
-      Zone.setZonesParamsFromRenderer(this);
-      Zone.setZone(Zone.types.ACTIVE, this.bounds.min.x, this.bounds.min.y, this.bounds.max.x, this.bounds.max.y);
-      Zone.setZone(Zone.types.RENDER, this.bounds.min.x, this.bounds.min.y, this.bounds.max.x, this.bounds.max.y);
-
+      StateManager.setRender(this);
+      Zone.updateBounds();
       Liquid.init(worldWidth, 64);
-      Render.init(this);
-
-      matter.after('Engine.update', function(){
-        Events.emit(Events.types.BEFORE_UPDATE);
-        Algorithm.update(deltaTime);
-        Render.update();
-        Events.emit(Events.types.AFTER_UPDATE);
-      });
-      // matter.after('Render.update', function () {
-      //   rendererUpdate();
-      // })
+      starter.next();
     });
     matter.after('World.create', function(this: Matter.World) {
-      console.log('World:');
-      console.dir(this);
-      State.setWorld(this);
-      State.setGravity(this.gravity.y, this.gravity.x);
+      StateManager.setWorld(this);
+      StateManager.setGravity(this.gravity.y, this.gravity.x);
+      starter.next();
     })
-    // matter.after('Engine.update', update)
+    matter.after('Engine.create', function(this: Matter.Engine) {
+      StateManager.setEngine(this);
+      starter.next();
+    })
+
+    function* start() {
+      yield 0;
+      yield 0;
+
+      console.log('State:');
+      console.dir(State);
+      Matter.Events.on(State.e, 'afterUpdate', function(){
+        Events.emit(Events.types.BEFORE_UPDATE);
+        Algorithm.update(deltaTime);
+        Events.emit(Events.types.AFTER_UPDATE);
+      })
+      Matter.Events.on(State.r, 'afterRender', function(){
+        Render.update();
+      })
+    }
   },
 };
 
