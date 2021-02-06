2c155bea6379b01639d9f7a431287b7fd7fee6f3
Alexemarg <alexemarg.dev@gmail.com>
2021-02-06 20:22:01 +0700

    Upd: state - add render & engine fields

--- src/state.ts
+++ src/state.ts
@@ -1,17 +1,30 @@
+import * as Render from './render';
+
 type TState = {
   w: Matter.World,
+  r: Matter.Render,
+  e: Matter.Engine,
   g: [number, number],
   h: number,
 }
 
 export const State: TState = {
   w: null,
+  r: null,
+  e: null,
   g: [0, 0.01],
   h: 30,
 };
 
 export function setWorld(world: Matter.World) {
   State.w = world;
+}
+export function setRender(render: Matter.Render) {
+  State.r = render;
+  Render.init(render);
+}
+export function setEngine(engine: Matter.Engine) {
+  State.e = engine;
 }
 export function setGravity(vertical: number, horizontal: number) {
   const divider = 10;
