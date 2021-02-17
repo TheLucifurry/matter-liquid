96dded4bb9c856174df9293cf218ec6963917b4a
Alexemarg <alexemarg.dev@gmail.com>
2021-02-17 15:44:41 +0700

    Ref: add enums file

---
+++ src/enums.ts
@@ -0,0 +1,14 @@
+export const EVENT_TYPES = {
+  PAUSED: '0',
+  CONTINUE: '1',
+}
+
+export const PARTICLE_PROPS = {
+  X: 0,
+  Y: 1,
+  PREV_X: 2,
+  PREV_Y: 3,
+  VEL_X:4,
+  VEL_Y: 5,
+  LIQUID_ID: 6,
+};
