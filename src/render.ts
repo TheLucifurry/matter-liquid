f674fd137de2aef5ae7dec76fffcc88a881cc215
Alexemarg <alexemarg.dev@gmail.com>
2021-02-21 13:37:37 +0700

    Ref: render - lite optimisations

--- src/render.ts
+++ src/render.ts
@@ -1,42 +1,42 @@
 import { PARTICLE_PROPS } from './enums';
-import { getRectWithPaddingsFromBounds, startViewTransform } from './utils';
+import { arrayEach, getRectWithPaddingsFromBounds, startViewTransform } from './utils';
 
 function getCoordsFromCellid(spatialHash: CSpatialHash, cellid: number): [number, number] {
   return [cellid % spatialHash._columns, Math.trunc(cellid / spatialHash._columns)];
 }
 
 function drawAtom(ctx: CanvasRenderingContext2D, x: number, y: number, color: string) {
-  const radius = 4;
+  const radius = 10;
   ctx.beginPath();
   ctx.fillStyle = color;
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.fill();
 }
 
-
-const colors: string[] = [];
-for (let i = 0; i < 1000; i++) {
-  colors.push('#'+Math.floor(Math.random()*16777215).toString(16));
-}
+// const colors: string[] = [];
+// for (let i = 0; i < 1000; i++) {
+//   colors.push('#'+Math.floor(Math.random()*16777215).toString(16));
+// }
 
 function renderGrid(store: TStore) {
   const ctx = store.render.context
   const cellSize = store.spatialHash.cellSize;
+  const csh = cellSize / 2;
 
   // @ts-ignore
   const hashCells: Array<[number, TSpatialHashItem[]]> = Object.entries(store.spatialHash.hash)
 
   ctx.textAlign = 'center';
+  ctx.lineWidth = 1;
+  ctx.fillStyle = 'white';
   for (let [cellid, cell] of hashCells) {
     const [cellX, cellY] = getCoordsFromCellid(store.spatialHash, cellid);
     const fX = cellX * cellSize;
     const fY = cellY * cellSize;
-    const csh = cellSize / 2;
 
     if(cell.length !== 0){
       ctx.strokeStyle = 'green';
-      ctx.fillStyle = 'white';
-      ctx.fillText('' + cell.length, fX+csh, fY+csh);
+      ctx.fillText('' + cell.length, fX + csh, fY + csh);
     }else{
       ctx.strokeStyle = 'gray';
     }
@@ -53,14 +53,14 @@ export default function update(liquid: CLiquid) {
   const ctx = Store.render.context;
   startViewTransform(Store.render);
 
-  // renderGrid(Store);
+  renderGrid(Store);
 
   const activeRect = getRectWithPaddingsFromBounds(Store.render.bounds, Store.activeBoundsPadding);
   const renderRect = getRectWithPaddingsFromBounds(Store.render.bounds, Store.renderBoundsPadding);
 
-  particles.forEach((part, id) => {
-    const x = part[PARTICLE_PROPS.X], y = part[PARTICLE_PROPS.Y];
+  arrayEach(particles, (part, id) => {
     if(!liquid.checkRectContainsParticle(renderRect, part))return;
+    const x = part[PARTICLE_PROPS.X], y = part[PARTICLE_PROPS.Y];
     const color = partColors.get(id) || liquids[part[PARTICLE_PROPS.LIQUID_ID]].color;
     drawAtom(ctx, x, y, color);
   })
