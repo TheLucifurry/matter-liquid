fa130a03c892bf772ef4434eea4ba801417a8f98
Alexemarg <alexemarg.dev@gmail.com>
2021-03-02 20:12:38 +0700

    utils - add vectorClampMaxLength()

--- src/utils.ts
+++ src/utils.ts
@@ -61,6 +61,13 @@ export function vectorMulVector(vec1: TVector, vec2: TVector): TVector {
 export function vectorSubVector(vec1: TVector, vec2: TVector): TVector {
   return [vec1[0] - vec2[0], vec1[1] - vec2[1]];
 }
+/** From three.js
+ * @see https://github.com/mrdoob/three.js/blob/be6a7dbb1b5247e7355fe6c5cb229b4597693f7e/src/math/Vector2.js#L269 */
+export function vectorClampMaxLength(vec: TVector, max: number): TVector {
+  const length = Math.hypot(vec[0], vec[1]);
+  const div = vectorDiv(vec, length || 1)
+  return vectorMul(div,  Math.max( 0, Math.min( max, length ) ) );
+}
 
 // Matter dependant
 export function getBodiesInRect(bodies: Matter.Body[], zone: TRect): Matter.Body[] {
