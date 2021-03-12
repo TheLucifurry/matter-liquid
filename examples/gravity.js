17d47238b7108d5b37563262f0a41cd8dafaaf91
Alexemarg <alexemarg.dev@gmail.com>
2021-03-12 15:23:28 +0700

    demos - add "Gravity" #18

---
+++ examples/gravity.js
@@ -0,0 +1,61 @@
+import { setWorldSize, setWorldBackground, init, cameraLookAt, initMouse, setDripper, getWorldParams } from './lib/fragments.js';
+import * as Util from './lib/utils.js';
+import Colors from './lib/colors.js';
+
+export default function () {
+  const { Liquid } = Matter;
+  const { engine, world, render, runner } = init();
+
+  const worldSize = 1024;
+  const color = Colors.getRandom();
+  const bgColor = Colors.getBackgroundFor(color);
+
+  setWorldSize(world, worldSize);
+  setWorldBackground(world, bgColor);
+  cameraLookAt(render, world.bounds);
+  const { mouseConstraint } = initMouse(render);
+  gravityControl(engine);
+
+  const liquid = Liquid.create({
+    engine,
+    render,
+    liquids: [{ color }], // Define one liquid
+    updateEveryFrame: 1,  // Set max 60 FPS
+  });
+  const { minX, maxX, minY, maxY, width } = getWorldParams(world);
+  const liquidCyanId = 0;
+  const seaHeight = 400;
+  liquid.fillZoneByLiquid(minX, maxY - seaHeight, width, seaHeight, liquidCyanId);
+
+  setDripper(render, liquid, mouseConstraint);
+
+  const caption = 'Arrow keys - direction | Space - disable';
+  document.getElementById('info').textContent = caption;
+
+  // For stats
+  window.ON_LIQUID_STARTED(liquid);
+
+  // context for MatterTools.Demo
+  return {
+    engine,
+    runner,
+    render,
+    canvas: render.canvas,
+    stop() {
+      Matter.Render.stop(render);
+      Matter.Runner.stop(runner);
+    },
+  };
+};
+
+
+function gravityControl(engine) {
+  const { gravity } = engine.world;
+  const defGravity = { x: 0, y: 0 };
+
+  Util.onkey(Util.KEY_CODES.UP, () => gravity.y = -1);
+  Util.onkey(Util.KEY_CODES.LEFT, () => gravity.x = -1);
+  Util.onkey(Util.KEY_CODES.DOWN, () => gravity.y = 1);
+  Util.onkey(Util.KEY_CODES.RIGHT, () => gravity.x = 1);
+  Util.onkey(Util.KEY_CODES.SPACE, () => Object.assign(gravity, defGravity));
+}
