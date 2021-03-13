48cc89bc9977b6e3552add3f70aac1b25106e7e1
Alexemarg <alexemarg.dev@gmail.com>
2021-03-13 20:21:19 +0700

    demos - add white color to palette

--- examples/lib/colors.js
+++ examples/lib/colors.js
@@ -1,12 +1,13 @@
 import { randomArrayItem } from './utils.js';
 
-const particlePalette = ['cyan', 'orange', 'lime', 'violet'];
+const particlePalette = ['cyan', 'orange', 'lime', 'violet', 'white'];
 
 const bgPaletteTable = {
   'cyan': '#050820',
   'orange': '#140B02',
   'lime': '#051102',
   'violet': '#150320',
+  'white': '#111',
 };
 
 export default {
