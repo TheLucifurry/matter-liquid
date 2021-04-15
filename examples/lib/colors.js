7815f549daa22dbbaa25ee45303cbdcb8f7abd84
Alexemarg <alexemarg.dev@gmail.com>
2021-04-15 14:02:27 +0700

    demos/colors - convert to hex

--- examples/lib/colors.js
+++ examples/lib/colors.js
@@ -4,11 +4,11 @@ function palette(particle, background, body) {
   return { particle, background, body };
 }
 const palettes = [
-  palette('cyan', '#050820', '#353860'),
-  palette('orange', '#140B02', '#342B22'),
-  palette('violet', '#150320', '#352340'),
-  palette('lime', '#051102', '#253122'),
-  palette('white', '#111', '#212121'),
+  palette('#0ff1', '#050820', '#353860'), // cyan
+  palette('#fa01', '#140B02', '#342B22'), // orange
+  palette('#e7e1', '#150320', '#352340'), // violet
+  palette('#0f01', '#051102', '#253122'), // lime
+  palette('#fff1', '#111', '#212121'),    // white
 ]
 let currentPaletteId = randomInt(0, palettes.length - 1);
 
