b7f3013d76587ef895718858abcb35bec7908e3d
Alexemarg <alexemarg.dev@gmail.com>
2021-04-23 14:05:59 +0700

    Add assemblyscript support

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
+import asl from '@assemblyscript/loader';
 import Package from '../package.json';
 import GlobalLiquid from './global/global';
 
-const MatterLiquid = {
-  name: Package.name,
-  version: Package.version,
-  for: 'matter-js@0.16.1',
-  // uses: [],
-  // options: {
-  //   something: true,
-  // },
-  install(matter: any) {
-    matter.Liquid = GlobalLiquid;
-  },
-};
+asl.instantiate(fetch('build/assembly.wasm'))
+  .then(({ exports }) => {
+    // @ts-ignore
+    window.ASSEMBLY = exports;
 
-// @ts-ignore
-Matter.Plugin.register(MatterLiquid);
+    const MatterLiquid = {
+      name: Package.name,
+      version: Package.version,
+      for: 'matter-js@0.16.1',
+      // uses: [],
+      // options: {
+      //   something: true,
+      // },
+      install(matter: any) {
+        matter.Liquid = GlobalLiquid;
+      },
+    };
+
+    // @ts-ignore
+    Matter.Plugin.register(MatterLiquid);
+  });
